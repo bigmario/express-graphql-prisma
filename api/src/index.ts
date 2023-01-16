9ffb3b08914f7c74ea5234a3b838fa03af59ee80
Mario Castro <mariocastro.pva@gmail.com>
2023-01-16 16:20:26 -0400

    server file

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,28 +1,5 @@
-import { ApolloServer } from 'apollo-server';
-import { resolvers } from './resolvers'
-import { loadFiles } from '@graphql-tools/load-files'
-import { PrismaClient } from '@prisma/client';
-import { typeDefs as scalarsTypeDefs, resolvers as scalarsResolvers } from 'graphql-scalars';
+import { serveGraphql } from './gqlServer';
 
-(async ()=>{
-  const prisma = new PrismaClient()
-
-  const typeDefs = [
-    ...await loadFiles('./src/**/*.graphql'),
-    scalarsTypeDefs
-  ]
-  const allResolvers = [
-    resolvers,
-    scalarsResolvers
-  ]
-
-  const server = new ApolloServer({
-    context: {
-      prisma
-    },
-    typeDefs,
-    resolvers: allResolvers
-  });
-
-  return server.listen().then(({url})=> console.log(`Server is running in ${url}`));
+(async () => {
+  await serveGraphql();
 })();
