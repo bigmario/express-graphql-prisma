706f7480529675e09a0bff218ccde8adf2281892
Mario Castro <mariocastro.pva@gmail.com>
2023-01-20 16:29:57 -0400

    hot fix

--- api/src/core/config/index.ts
+++ api/src/core/config/index.ts
@@ -7,12 +7,12 @@ export const config = {
   isProd: process.env.NODE_ENV === 'production',
   isDev: process.env.NODE_ENV === 'development',
   devPort: process.env.DEV_PORT,
-  prodPort: process.env.PROD_PORT,
+  prodPort: process.env.PORT,
   dbUrl: process.env.DATABASE_URL,
   apiKey: process.env.API_KEY,
   jwtSecret: process.env.JWT_SECRET,
   smtpEmail: process.env.SMTP_EMAIL,
   smtpPassword: process.env.SMTP_PASSWORD,
-  smtpPort: process.env.SMTP_PORT || 3000,
+  smtpPort: process.env.SMTP_PORT || 2525,
   smtpHost: process.env.SMTP_HOST
 }
